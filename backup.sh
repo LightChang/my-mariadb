c96ebef78fc8624cb852bbc5cf07e9af4f245e65
Light <lightman.chang@gmail.com>
2024-12-26 23:26:37 +0800

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -11,32 +11,40 @@ until mysql -h "$DB_HOST" -u "$ROOT_USER" -p"$ROOT_PASSWORD" -e "select 1" > /de
   echo "等待 MySQL 伺服器啟動中..."
   sleep 5
 done
-echo "MySQL 伺服器已啟動，開始備份..."
+echo "MySQL 伺服器已啟動"
 
-# 設定日期格式
-DATE=$(date +%Y%m%d_%H%M)
+# 開始循環備份
+while true; do
+  echo "開始新一輪備份..."
 
-# 刪除過舊的執行紀錄檔
-find /log -type f -name "*.log" -mtime +30 -exec rm -f {} \;
+  # 設定日期格式
+  DATE=$(date +%Y%m%d_%H%M)
 
-# 取得所有非系統使用者
-USERS=$(mysql -h $DB_HOST -u$ROOT_USER -p$ROOT_PASSWORD -se "SELECT user FROM mysql.user WHERE user NOT IN ('root', 'healthcheck', 'mariadb.sys')" 2>&1 | grep -v "Warning")
+  # 刪除過舊的執行紀錄檔
+  find /log -type f -name "*.log" -mtime +30 -exec rm -f {} \;
 
-# 遍歷每個使用者
-for USER in $USERS; do
-  echo ''
-  echo '========='
-  echo $USER;
-  echo '========='
+  # 取得所有非系統使用者
+  USERS=$(mysql -h $DB_HOST -u$ROOT_USER -p$ROOT_PASSWORD -se "SELECT user FROM mysql.user WHERE user NOT IN ('root', 'healthcheck', 'mariadb.sys')" 2>&1 | grep -v "Warning")
 
-  # 取得該使用者有權限的資料庫
-  DBS=$(mysql -h $DB_HOST -u$ROOT_USER -p$ROOT_PASSWORD -se "SELECT DISTINCT table_schema FROM information_schema.SCHEMA_PRIVILEGES WHERE grantee = CONCAT(\"'\", '$USER', \"'@'\", '%', \"'\")" 2>&1 | grep -v "Warning" | grep -Ev '(information_schema|performance_schema|mysql|sys)')
+  # 遍歷每個使用者
+  for USER in $USERS; do
+    echo ''
+    echo '========='
+    echo $USER;
+    echo '========='
 
-  # 為每個資料庫進行備份
-  for DB in $DBS; do
-      USER_BACKUP_DIR="$BACKUP_DIR/$USER"
-      mkdir -p $USER_BACKUP_DIR
-      mysqldump --column-statistics=0 -h $DB_HOST -u$ROOT_USER -p$ROOT_PASSWORD $DB > $USER_BACKUP_DIR/${DB}_${DATE}.sql 2>&1 | grep -v "Warning"
-      echo "備份完成：$DB"
+    # 取得該使用者有權限的資料庫
+    DBS=$(mysql -h $DB_HOST -u$ROOT_USER -p$ROOT_PASSWORD -se "SELECT DISTINCT table_schema FROM information_schema.SCHEMA_PRIVILEGES WHERE grantee = CONCAT(\"'\", '$USER', \"'@'\", '%', \"'\")" 2>&1 | grep -v "Warning" | grep -Ev '(information_schema|performance_schema|mysql|sys)')
+
+    # 為每個資料庫進行備份
+    for DB in $DBS; do
+        USER_BACKUP_DIR="$BACKUP_DIR/$USER"
+        mkdir -p $USER_BACKUP_DIR
+        mysqldump --column-statistics=0 -h $DB_HOST -u$ROOT_USER -p$ROOT_PASSWORD $DB > $USER_BACKUP_DIR/${DB}_${DATE}.sql 2>&1 | grep -v "Warning"
+        echo "備份完成：$DB"
+    done
   done
+
+  echo "本輪備份完成，等待下次備份..."
+  sleep $((${EVERY_N_HR_BACKUP} * 3600))
 done
